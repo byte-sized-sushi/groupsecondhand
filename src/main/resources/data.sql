b878e635944162242b0b6def241e65f92837ef6f
byte-sized-sushi <bytesizedsushi@gmail.com>
2022-10-11 10:40:49 +0200

    La till lite mer i databasen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO PRODUCT (NAME, PRICE, CATEGORY, IMAGE) VALUES ('Testnamn',299,'SciFi','/images/sushi.png'),
-('Testnamn2',349,'Fantasy', '/images/sushi.png');
+('Tale of Sushi',349,'Fantasy', '/images/sushi.png'),
+('Pippi Långstrump',150,'Fantasy', '/images/pippi.jpg'),
+('the Hitchhikers Guide to the Galaxy',249,'Fantasy', '/images/thehitchhikers.jpg'),
+('the Iliad',34999,'Fantasy', '/images/theiliad.jpg');
